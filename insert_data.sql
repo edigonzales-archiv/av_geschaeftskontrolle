cb64ac7c48b567d77b561ac2e6469b6ff321af6e
edi.gonzales@gmail.com <edi.gonzales@gmail.com>
2013-12-19 11:20:37 +0100

    wieder mal eim commit...

---
+++ insert_data.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO av_geschaeftskontrolle.rechnungsjahr VALUES (2, 2013);
+INSERT INTO av_geschaeftskontrolle.rechnungsjahr VALUES (3, 2014);
+INSERT INTO av_geschaeftskontrolle.rechnungsjahr VALUES (4, 2015);
+INSERT INTO av_geschaeftskontrolle.rechnungsjahr VALUES (5, 2016);
+INSERT INTO av_geschaeftskontrolle.rechnungsjahr VALUES (6, 2017);
+
+INSERT INTO av_geschaeftskontrolle.konto VALUES (1, 3130015, 'Unterhalt und Nachführung', '');
+INSERT INTO av_geschaeftskontrolle.konto VALUES (2, 5640000, 'Oeffentl. Unternehmungen', 'RADAV-Konto');
+
+INSERT INTO av_geschaeftskontrolle.plankostenkonto VALUES (1, 2, 400000.00, 2014, 'RADAV');
+INSERT INTO av_geschaeftskontrolle.plankostenkonto VALUES (2, 1, 700000.00, 2014, NULL);
+
+INSERT INTO av_geschaeftskontrolle.unternehmer VALUES (1, 'W+H AG', NULL, 'Meile', 'Reto', 'Blümlisalpstrasse ', '6', 4562, 'Biberist', NULL);
+INSERT INTO av_geschaeftskontrolle.unternehmer VALUES (2, 'BSB + Partner, Ingenieure und Planer', NULL, 'Schor', 'Urs', 'Von Roll-Strasse', '29', 4702, 'Oensingen', NULL);
+INSERT INTO av_geschaeftskontrolle.unternehmer VALUES (3, 'Emch + Berger AG Vermessungen', NULL, 'Cantaluppi', 'Dominik', 'Schöngrünstrasse ', '35', 4500, 'Solothurn', NULL);
