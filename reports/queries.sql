47d2c8a90cb6eb9460b1f44ff3468a46cf3dd536
Stefan Ziegler <stefan@hatschepsut.(none)>
2013-12-22 19:57:40 +0100

    start mit queries für reports

---
+++ reports/queries.sql
@@ -0,0 +1,10 @@
+﻿-- to_char(12454.8, E'999\'999\'999\'999D99S')
+
+-- Alle laufenden Projekte.
+-- Projekte ohne 'datum_ende'
+
+SELECT proj.*, konto.nr, konto."name" 
+FROM av_geschaeftskontrolle.projekt as proj, av_geschaeftskontrolle.konto as konto
+WHERE konto.id = proj.konto_id
+AND datum_ende IS NULL or trim('' from datum_ende::text) = ''
+ORDER BY datum_start, proj."name";
